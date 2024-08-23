86520fed76d2ce91ff8168d3866bb3e5181218e0
Raigis <alerish015@gmail.com>
2024-08-23 22:35:40 +0300

    Complete third task

--- ThirdTask/main.cpp
+++ ThirdTask/main.cpp
@@ -9,13 +9,51 @@
 #include <iostream>
 #include <vector>
 
-std::vector<int> checking(std::string ip) {
-    
+std::vector<std::string> separation(std::string ip){
+    std::vector<std::string> vecIp;
+    std::string octet;
+    for (int i = 0; i < ip.length(); i++) {
+        if (ip[i] == '.' ) {
+            vecIp.push_back(octet);
+            octet = "";
+        } else {
+            octet += ip[i];
+        }
+    }
+    vecIp.push_back(octet);
+    return vecIp;
 }
+
+int conversion(std::string octet) {
+    while (octet.length() != 3) {
+        octet = '0' + octet;
+    }
+    return ((octet[0] - '0') * 100 + (octet[1] - '0') * 10 + (octet[2] - '0'));
+}
+
+bool checking(std::string ip) {
+    std::vector<std::string> vecIp = separation(ip);
+    if (vecIp.size() != 4) return false;
+    for (std::string octet : vecIp) {
+        if (octet.length() < 1 || octet.length() > 3 || (octet.length() > 1 && octet[0] == '0')) return false;
+        for (char i : octet){
+            if (i < '0' || i > '9') return false;
+        }
+        int octetInt = conversion(octet);
+        if(octetInt < 0 || octetInt > 255) return false;
+    }
+    return true;
+}
+
 
 int main() {
     std::string ip;
     std::cout << "Enter the ip address:\n";
     std::getline(std::cin, ip);
-    std::vector<int> result = checking(ip);
+    bool result = checking(ip);
+    if (result) {
+        std::cout << "\nValid." << std::endl;
+    } else {
+        std::cout << "\nInvalid." << std::endl;
+    }
 }
